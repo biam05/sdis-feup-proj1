5b8179c53f4f4417b9d5ef8e25799946251def32
Beatriz Mendes <45185351+biam05@users.noreply.github.com>
2021-04-10 14:09:46 +0100

    changed ports (need to be >5000)

--- proj1/start.sh
+++ proj1/start.sh
@@ -4,19 +4,19 @@ echo "--------- Starting RMI Registry ---------"
 start rmiregistry -J-Djava.class.path=src/out/production/proj1
 
 echo "--------- Starting Peer 1 ---------"
-start java -Djava.rmi.server.codebase=src/out/production/proj1/sdis/t1g06/ -Djava.security.manager -Djava.security.policy=src/rmipolicy/my.policy/ -classpath src/out/production/proj1 sdis.t1g06.Peer 1.0 1 ServiceInterface 224.0.0.1 4441 224.0.0.2 4442 224.0.0.3 4443
+start java -Djava.rmi.server.codebase=src/out/production/proj1/sdis/t1g06/ -Djava.security.manager -Djava.security.policy=src/rmipolicy/my.policy/ -classpath src/out/production/proj1 sdis.t1g06.Peer 1.0 1 ServiceInterface 230.0.0.1 5001 230.0.0.2 5002 230.0.0.3 5003
 
 echo "--------- Starting Peer 2 ---------"
-start java -Djava.rmi.server.codebase=src/out/production/proj1/sdis/t1g06/ -Djava.security.manager -Djava.security.policy=src/rmipolicy/my.policy/ -classpath src/out/production/proj1 sdis.t1g06.Peer 1.0 2 ServiceInterface 224.0.0.1 4441 224.0.0.2 4442 224.0.0.3 4443
+start java -Djava.rmi.server.codebase=src/out/production/proj1/sdis/t1g06/ -Djava.security.manager -Djava.security.policy=src/rmipolicy/my.policy/ -classpath src/out/production/proj1 sdis.t1g06.Peer 1.0 2 ServiceInterface 230.0.0.1 5001 230.0.0.2 5002 230.0.0.3 5003
 
 echo "--------- Starting Peer 3 ---------"
-start java -Djava.rmi.server.codebase=src/out/production/proj1/sdis/t1g06/ -Djava.security.manager -Djava.security.policy=src/rmipolicy/my.policy/ -classpath src/out/production/proj1 sdis.t1g06.Peer 1.0 3 ServiceInterface 224.0.0.1 4441 224.0.0.2 4442 224.0.0.3 4443
+start java -Djava.rmi.server.codebase=src/out/production/proj1/sdis/t1g06/ -Djava.security.manager -Djava.security.policy=src/rmipolicy/my.policy/ -classpath src/out/production/proj1 sdis.t1g06.Peer 1.0 3 ServiceInterface 230.0.0.1 5001 230.0.0.2 5002 230.0.0.3 5003
 
 echo "--------- Starting Peer 4 ---------"
-start java -Djava.rmi.server.codebase=src/out/production/proj1/sdis/t1g06/ -Djava.security.manager -Djava.security.policy=src/rmipolicy/my.policy/ -classpath src/out/production/proj1 sdis.t1g06.Peer 1.0 4 ServiceInterface 224.0.0.1 4441 224.0.0.2 4442 224.0.0.3 4443
+start java -Djava.rmi.server.codebase=src/out/production/proj1/sdis/t1g06/ -Djava.security.manager -Djava.security.policy=src/rmipolicy/my.policy/ -classpath src/out/production/proj1 sdis.t1g06.Peer 1.0 4 ServiceInterface 230.0.0.1 5001 230.0.0.2 5002 230.0.0.3 5003
 
 echo "--------- Starting Peer 5 ---------"
-start java -Djava.rmi.server.codebase=src/out/production/proj1/sdis/t1g06/ -Djava.security.manager -Djava.security.policy=src/rmipolicy/my.policy/ -classpath src/out/production/proj1 sdis.t1g06.Peer 1.0 5 ServiceInterface 224.0.0.1 4441 224.0.0.2 4442 224.0.0.3 4443
+start java -Djava.rmi.server.codebase=src/out/production/proj1/sdis/t1g06/ -Djava.security.manager -Djava.security.policy=src/rmipolicy/my.policy/ -classpath src/out/production/proj1 sdis.t1g06.Peer 1.0 5 ServiceInterface 230.0.0.1 5001 230.0.0.2 5002 230.0.0.3 5003
 
 sleep 0.5
 
